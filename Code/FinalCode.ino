e432af961998eeabef418ac9dd04b7f5ea3d8f48
Maria-Anastasia Moustaka <mariamou7@windowslive.com>
2022-03-14 12:45:45 +0200

    Create FinalCode.ino

---
+++ Code/FinalCode.ino
@@ -0,0 +1,19 @@
+/*
+  Smart ecogarbage bins final code
+
+  Author: Smart ecogarbage bins team
+*/
+
+//Initializations
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+
+}
